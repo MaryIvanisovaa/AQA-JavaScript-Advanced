affa58505cded725b34dcc48c996154211b3515b
MaryIvanisovaa <ivanisovamaria07@gmail.com>
2023-09-22 20:57:45 +0300

    I have done homework which includes 5 exercise

---
+++ js-variables/variables.js
@@ -0,0 +1,20 @@
+let adminName ='Maria';
+let userName ='Igor';
+
+console.log(typeof adminName, typeof userName);
+
+let feedback = "awesome";
+let color = 'orange';
+let number = 6
+
+console.log(`Javascript is: ${feedback}, ${color}, ${number}`);
+
+let size = 46; 
+size = 42;
+console.log(size);
+
+
+
+
+
+
